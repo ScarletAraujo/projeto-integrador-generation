cae1a43d5969769a29728d2c182f3c681f1ab22f
IgorPV <igor.pereira.vieira@gmail.com>
2021-03-22 11:03:37 -0300

    Adicionando projeto maven

---
+++ EducaTI/src/test/java/com/educati/EducaTI/EducaTiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.educati.EducaTI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducaTiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
