8d7c4c747e1de5fe7175e7e28545aa130b6fd9da
ScarletAraujo <scarlet.araujo@gmail.com>
2021-03-22 15:09:25 -0300

    model Usuario

--- EducaTI/src/main/java/com/educati/EducaTI/model/Usuario.java
+++ EducaTI/src/main/java/com/educati/EducaTI/model/Usuario.java
@@ -1,5 +1,84 @@
 package com.educati.EducaTI.model;
 
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "Usuario", uniqueConstraints= @UniqueConstraint(columnNames={"nome", "email", "CPF"}))
+
+
 public class Usuario {
+	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	@Size(min = 5, max = 256)
+	private String nome;
+	
+	@NotNull
+	@Size(min = 10, max = 500)
+	private String email;
+	
+	@NotNull
+	@Size (min = 6, max = 1000)
+	private String senha;
+	
+	@NotNull
+	@Size (min = 11, max = 11)
+	private int CPF;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public int getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(int cPF) {
+		CPF = cPF;
+	} 
+	
+	
+	
 
 }
