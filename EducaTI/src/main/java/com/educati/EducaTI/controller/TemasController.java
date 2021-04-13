918f65c19c653737d40b72dd474109bb050522d1
unknown <richardcf1998@gmail.com>
2021-04-13 15:38:56 -0300

    Swagger e @ApiOperation

--- EducaTI/src/main/java/com/educati/EducaTI/controller/TemasController.java
+++ EducaTI/src/main/java/com/educati/EducaTI/controller/TemasController.java
@@ -18,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.educati.EducaTI.model.Temas;
 import com.educati.EducaTI.repository.TemasRepository;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RequestMapping("/tema")
@@ -29,38 +31,50 @@ public class TemasController {
 	
 	
 	@GetMapping
+	@ApiOperation(value="Retorna lista de Temas")
 	public ResponseEntity<List<Temas>>getAll(){
 		return ResponseEntity.ok(repository.findAll());
 	}
+	
 	@GetMapping("/{id}")
+	@ApiOperation(value="Retorna lista de Temas por id")
 	public ResponseEntity<Temas>getById(@PathVariable Long id){
 		return repository.findById(id).map(tema -> ResponseEntity.ok(tema))
 				.orElse(ResponseEntity.notFound().build());
 	}
+	
 	@GetMapping("/titulo/{titulo}")
+	@ApiOperation(value="Retorna lista de Temas por Titulo")
 	public ResponseEntity<List<Temas>>getByTitulo(@PathVariable String titulo){
 		return ResponseEntity.ok(repository.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
 	@GetMapping("/texto/{texto}")
+	@ApiOperation(value="Retorna lista de Temas por trecho específico de texto")
 	public ResponseEntity<List<Temas>>getByTexto(@PathVariable String texto){
 		return ResponseEntity.ok(repository.findAllByTextoContainingIgnoreCase(texto));
 	}
+	
 	@GetMapping("/nivel/{nivel}")
+	@ApiOperation(value="Retorna lista de Temas por nivel")
 	public ResponseEntity<List<Temas>>getByNivel(@PathVariable String nivel){
 		return ResponseEntity.ok(repository.findAllByNivelContainingIgnoreCase(nivel));
 	}
 	
-	
-	
 	@PostMapping
+	@ApiOperation(value="Cria Temas")
 	public ResponseEntity<Temas> post (@RequestBody Temas temas){
 		return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(temas));
 	}
+	
 	@PutMapping
+	@ApiOperation(value="Atualiza dados de Temas")
 	public ResponseEntity<Temas> put (@RequestBody Temas temas){
 		return  ResponseEntity.ok(repository.save(temas));
 	}
+	
 	@DeleteMapping("/{id}")
+	@ApiOperation(value="Deleta Temas por id")
 	public void delete(@PathVariable Long id) {
 		repository.deleteById(id);
 	}
